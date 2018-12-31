d64facf506ebf464738d89a7f110ce90f72b936f
umihico <umihico@users.noreply.github.com>
2018-12-31 21:17:37 +0900

    learned about executors and lambda

---
+++ java/ExecutorsStudy.java
@@ -0,0 +1,23 @@
+// how to run in cmd.exe
+// javac CpuTest.java & java CpuTest
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExecutorsStudy {
+        public static void main(String... args){
+                int vcpu = Runtime.getRuntime().availableProcessors();
+                System.out.println("vCPU: " + vcpu);
+                ExecutorService service = Executors.newFixedThreadPool(vcpu);
+                List<Future<Double> > futures = new ArrayList<>();
+                for(int i = 0; i < 100000; i++) {
+                        futures.add(service.submit(()->IntStream.range(1, 100000).mapToDouble(Math::sin).sum()));
+                }
+                service.shutdown();
+                System.out.println("dummy: " + futures.stream().mapToDouble(future->{
+                        try{ return future.get(); }catch(Exception e) { return 0.0d; }
+                }).sum());
+        }
+}
