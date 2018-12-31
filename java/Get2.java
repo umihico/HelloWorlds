1b87d7a32a88342291fac6551cef37349059337f
umihico <umihico@users.noreply.github.com>
2018-12-31 16:20:05 +0900

    add statuscode method

--- java/Get2.java
+++ java/Get2.java
@@ -1,14 +1,13 @@
-// credit:https://stackoverflow.com/questions/1485708/how-do-i-do-a-http-get-in-java
-
-// how to run in cmd.exe
-// javac Get.java & java Get https://stackoverflow.com/questions/1485708/how-do-i-do-a-http-get-in-java
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.HttpURLConnection;
+// credit:https://stackoverflow.com/questions/1485708/how-do-i-do-a-http-get-in-java
+
+// how to run in cmd.exe
+// javac Get2.java & java Get2 https://stackoverflow.com/questions/1485708/how-do-i-do-a-http-get-in-java
 
 public class Get2 {
-
         public static String getHTML(String TargetURL) throws Exception {
                 StringBuilder result = new StringBuilder();
                 URL url = new URL(TargetURL);
@@ -20,7 +19,10 @@ public class Get2 {
                         result.append(line);
                 }
                 rd.close();
+                int code = conn.getResponseCode();
+                System.out.println(code);
                 return result.toString();
+
         }
 
         public static void main(String[] args) throws Exception
