2457e4df1f4c69a14b6e1f5d0851e6e8ae84f3c4
umihico <umihico@users.noreply.github.com>
2019-01-04 16:43:13 +0900

    learn http request

---
+++ JavaScript/httpget.js
@@ -0,0 +1,33 @@
+// const Http = new XMLHttpRequest();
+// const url='https://api.github.com/users/umihico/repos?per_page=100&page=1';
+// Http.open("GET", url);
+// Http.send();
+// Http.onreadystatechange=(e)=>{
+// console.log(Http.responseText)
+// }
+var url; // リクエスト先URL
+var request;
+request = new ActiveXObject("MSXML2.XMLHTTP");
+// request = new XMLHttpRequest();
+// request = new ActiveXObject("Microsoft.XMLHTTP");
+// if( window.XMLHttpRequest){
+// }else if(window.ActiveXObject){
+//   try {
+//   } catch (e) {
+//   }
+// }
+request.open('GET', 'https://api.github.com/users/umihico/repos?per_page=100&page=1');
+request.onreadystatechange = function () {
+    if (request.readyState != 4) {
+      console.log('a')
+        // リクエスト中
+    } else if (request.status != 200) {
+      console.log('a')
+        // 失敗
+    } else {
+      console.log(Http.responseText)
+        // 取得成功
+        // var result = request.responseText;
+    }
+};
+request.send(null);
