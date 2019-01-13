545dea2311da00e5ce0ac05772820a00aed5feb2
umihico <umihico@users.noreply.github.com>
2019-01-13 16:53:05 +0900

    learn handling how to prevent missing args

---
+++ Shell/handle-args-missing.sh
@@ -0,0 +1,9 @@
+echo $#
+if [ $# -lt 3 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  exit 2
+elif [ $# -gt 3 ]; then
+  echo 1>&2 "$0: too many arguments"
+  exit 2
+fi
+# The three arguments are available as "$1", "$2", "$3"
