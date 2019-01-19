846729768e151c618ed78844d691dd9361cebacb
umihico <umihico@users.noreply.github.com>
2019-01-20 08:58:56 +0900

    success save png

--- Python/learn_screenshot.py
+++ Python/learn_screenshot.py
@@ -4,6 +4,8 @@ chrome = Chrome()
 chrome.get("https://github.com/umihico")
 result = chrome.get_screenshot_as_png()
 print(len(result), type(result), result[:30])
+with open('test.png', 'wb') as f:
+    f.write(result)
 # 227427 <class 'bytes'> b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x06\x12\x00\x00\x04\x91\x08\x06\x00\x00\x00\xd2'
 result = chrome.get_screenshot_as_base64()
 print(len(result), type(result), result[:30])
